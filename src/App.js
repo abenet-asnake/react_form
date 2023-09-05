302f875fe56af3954dd6083b483ea853cbf65e7e
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-09-05 13:14:30 +0300

    creating all file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-
+import './App.css'
 function App() {
   return (
     <div className="App">
- 
-        <p>
-         HELLO WORLD
-
-        </p>
+      <form>
         
-      
+      </form> 
     </div>
   );
 }
