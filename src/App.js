3e40993af5587a6f738d8cfbfd6a7de5bdecb482
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-09-05 14:09:04 +0300

    Creating Properties by using palceholder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
+import FormInput from './componnet/FormInput';
 function App() {
   return (
     <div className="App">
       <form>
-        
+        <FormInput placeholder="Enter Your Full Name"/>
+        <FormInput placeholder="Enter Your User Name"/>
+        <FormInput placeholder="Enter Your Password"/>
+        <FormInput placeholder="Rept Your Password "/>
       </form> 
     </div>
   );
---
+++ src/componnet/FormInput.js
@@ -0,0 +1,13 @@
+import './FormInput.css';
+const FormInput =  (props) => {
+   return(
+    <div className="formInput">
+          <label>Full Name</label>
+          <input placeholder={props.placeholder}/>
+          
+          
+     </div>
+   )
+}
+
+export default FormInput;
